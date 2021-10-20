5e5adf26b35a43405f1c63beaeb4b635083513c4
shunt16 <sam.hunt@npl.co.uk>
2021-10-20 18:55:40 +0100

    linting with black

--- setup.py
+++ setup.py
@@ -8,31 +8,31 @@ from setuptools import find_packages, setup
 # but avoids importing the module which may not be installed yet:
 __version__ = None
 here = path.abspath(path.dirname(__file__))
-with open('dsbuilder/version.py') as f:
+with open("dsbuilder/version.py") as f:
     exec(f.read())
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-setup(name='dsbuilder',
-      version=__version__,
-      description='creating datasets, to specification',
-      authors='Sam Hunt',
-      email='sam.hunt@npl.co.uk',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url='https://dsbuilder.readthedocs.io',
-      classifiers=[
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent",
-        ],
-      python_requires='>=3.6',
-      packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-      install_requires=[],
-      entry_points={
-          'console_scripts': [
-          ],
-                    }
-      )
+setup(
+    name="dsbuilder",
+    version=__version__,
+    description="creating datasets, to specification",
+    authors="Sam Hunt",
+    email="sam.hunt@npl.co.uk",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://dsbuilder.readthedocs.io",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+    install_requires=[],
+    entry_points={
+        "console_scripts": [],
+    },
+)
